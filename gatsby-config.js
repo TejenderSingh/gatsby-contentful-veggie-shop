8ad13e62fa22cbc8255e69d4ad87f1a801b70aac
TejenderSingh <singhtj22@gmail.com>
2019-08-21 01:44:19 +1200

    Connected to Contentful CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,13 @@ module.exports = {
         // Add any options here
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
